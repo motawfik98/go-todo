e41dea33425ed90c6605ee4a8980bd45ff7b27a4
Mohamed Ahmed <motawfik1998@gmail.com>
2019-04-22 16:35:42 +0200

    Added fetchAllTodo function

--- handlers.todo.go
+++ handlers.todo.go
@@ -14,3 +14,19 @@ func createTodo(c *gin.Context) {
     gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "resourceId": todo.ID},
   )
 }
+
+func fetchAllTodo(c *gin.Context) {
+  var todos []todoModel
+  var _todos []todoModel
+
+  db.Find(&todos)
+
+  if len(todos) <= 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+    return
+  }
+  for _, item := range todos {
+    _todos = append(_todos, todoModel{Title: item.Title, Completed: item.Completed})
+ }
+ c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
+}
