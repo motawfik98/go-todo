f57983107638f2f4bf4e55243ef467cd669a3d32
Mohamed Ahmed <motawfik1998@gmail.com>
2019-04-22 21:48:55 +0200

    Added fetchSingleTodo method

--- handlers.todo.go
+++ handlers.todo.go
@@ -30,3 +30,14 @@ func fetchAllTodo(c *gin.Context) {
  }
  c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
 }
+
+func fetchSingleTodo(c *gin.Context) {
+  var todo todoModel
+  todoID := c.Param("id")
+  db.First(&todo, todoID)
+  if todo.ID == 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+    return
+  }
+  c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
+}
