20356086db83905fffc2a8c9a765b94cd4af8fc5
Mohamed Ahmed <motawfik1998@gmail.com>
2019-04-22 22:00:51 +0200

    Added updateTodo method

--- handlers.todo.go
+++ handlers.todo.go
@@ -41,3 +41,19 @@ func fetchSingleTodo(c *gin.Context) {
   }
   c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": todo})
 }
+
+func updateTodo(c *gin.Context) {
+  var todo todoModel
+  todoID := c.Param("id")
+
+  db.First(&todo, todoID)
+  if todo.ID == 0 {
+    c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+    return
+  }
+
+  db.Model(&todo).Update("title", c.PostForm("title"))
+  completed, _ := strconv.Atoi(c.PostForm("completed"))
+  db.Model(&todo).Update("completed", completed)
+  c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully!"})
+}
